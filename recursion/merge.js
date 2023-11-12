8153da7f636ceb2a30f8cfd0d4a4d77fd853b8f1
Jesse Farmer <jesse@20bits.com>
2023-11-12 12:16:04 -0800

    [recursion] add merge.js

---
+++ recursion/merge.js
@@ -0,0 +1,24 @@
+let {isEmpty, prepend, unprepend} = require('./lists');
+
+function merge(left, right) {
+  if (isEmpty(left)) {
+    return right;
+  }
+
+  if (isEmpty(right)) {
+    return left;
+  }
+
+  let [leftFirst, leftRest] = unprepend(left);
+  let [rightFirst, rightRest] = unprepend(right);
+
+  if (leftFirst < rightFirst) {
+    return prepend(leftFirst, merge(leftRest, right));
+  } else {
+    return prepend(rightFirst, merge(left, rightRest));
+  }
+}
+
+module.exports = {
+  merge,
+};
