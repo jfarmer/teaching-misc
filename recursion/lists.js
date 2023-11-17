2b5779194daa846e19f8babb03070ff9b9a58b8f
Jesse Farmer <jesse@20bits.com>
2023-11-17 10:59:46 -0800

    [recursion] add comments to lists.js and add coderpad.js

--- recursion/lists.js
+++ recursion/lists.js
@@ -3,6 +3,9 @@ const util = require('util');
 const EMPTY_LIST = null;
 
 class Node {
+  /**
+   * Helper function to construct a linked list from an array of values
+   */
   static fromArray(array) {
     if (array.length === 0) {
       return EMPTY_LIST;
@@ -39,6 +42,10 @@ class Node {
   }
 }
 
+/**
+ * Helper function to pretty-print linked lists. Defined outside
+ * the Node class so that we can use `null` to represent the empty list.
+ */
 function listToString(list) {
   if (isEmpty(list)) {
     return '()';
@@ -49,14 +56,24 @@ function listToString(list) {
   return `${first} -> ${listToString(rest)}`;
 }
 
+/**
+ * Returns true if list is empty and false otherwise
+ */
 function isEmpty(list) {
   return list === EMPTY_LIST;
 }
 
+/**
+ * Returns true if rest of list is empty and false otherwise
+ * That is, returns true if list has only one element
+ */
 function isRestEmpty(list) {
   return isEmpty(list) || isEmpty(list.next);
 }
 
+/**
+ * Construct a new list by prepending a value to another list
+ */
 function prepend(val, list) {
   if (isEmpty(list)) {
     return new Node(val);
@@ -65,48 +82,12 @@ function prepend(val, list) {
   return new Node(val, list);
 }
 
+/**
+ * Returns a pair consisting of the first element of the list and the rest of the list
+ */
 function unprepend(list) {
   return [list.value, list.next];
 }
-
-// Left vs right fold
-//
-// foldLeft([10, 20, 30], add, 0)  == add(add(add(0, 10), 20), 30)
-//                                 == (((0 + 10) + 20) + 30)
-//
-// foldRight([10, 20, 30], add, 0) == add(10, add(20, add(30, 0)))
-//                                 == (10 + (20 + (30 + 0)))
-//
-// foldLeft and foldRight differ in how they group operations w/ parantheses, i.e.,
-// which pairwise operations are performed first.
-// foldLeft performs operation starting from left-most elements
-// foldRight performs operation starting from right-most elements
-//
-// If fn is associative then foldLeft and foldRight will return the same result
-// Otherwise, one is often more efficient than the other depending on what structure
-// you're folding over.
-//
-// Linked lists like "prepend" and foldRight is prepend-friendly
-// Arrays like "append" and foldLeft is append-friendly
-
-
-// let list = Node.fromArray([10, 21, 30, -17, 40, -109]);
-// let list2 = Node.fromArray(['one', 'two']);
-
-// console.log('list:    ', list);
-// console.log('sum:     ', sum(list));
-// console.log('max:     ', max(list));
-// console.log('odds:    ', filter(list, x => x % 2 !== 0));
-// console.log('double:  ', map(list, x => x * 2));
-// console.log('everyNeg:', every(list, x => x < 0));
-// console.log('someNeg: ', some(list, x => x < 0));
-// console.log('maxByAbs:', maxBy(list, x => Math.abs(x)));
-// console.log('reverse: ', reverse(list));
-// console.log('revBad:  ', reverseBad(list));
-// console.log('reverseA:', reverseA(list));
-// console.log('append:  ', append(list, 200));
-// console.log('zip:     ', zip(list, list2));
-// console.log('groupBy: ', groupBy(list, x => x % 2 === 0));
 
 module.exports = {
   Node,
