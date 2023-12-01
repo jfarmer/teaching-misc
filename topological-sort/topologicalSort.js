e147aafba8bc220ed43241729a4bce84378f1101
Jesse Farmer <jesse@20bits.com>
2023-12-01 10:42:23 -0800

    [topological-sort] Add descriptions to functions + slight refactoring

--- topological-sort/topologicalSort.js
+++ topological-sort/topologicalSort.js
@@ -1,3 +1,7 @@
+/**
+ * Given a graph represented as a vertex list and an edge list,
+ * return an adjacency list that represents the same graph
+ */
 function graphToAdjacencyList(vertexList, edgeList) {
   const adjacencyList = {};
 
@@ -12,26 +16,40 @@ function graphToAdjacencyList(vertexList, edgeList) {
   return adjacencyList;
 }
 
+/**
+ * Given a graph return a list of its vertexes
+ */
 function graphGetVertexes(graph) {
   return Object.keys(graph);
 }
 
-function graphGetNeighbors(graph, vertex) {
-  return graph[vertex];
-}
-
+/**
+ * Given a graph, return an array of source vertexes, i.e., vertexes
+ * with no incoming edges
+ */
 function graphGetSources(graph) {
-  let nonSources = new Set(Object.values(graph).flat());
+  let inDegrees = graphGetInDegrees(graph);
 
-  return Object.keys(graph).filter(vertex => !nonSources.has(vertex));
+  return graphGetVertexes(graph).filter(vertex => inDegrees.get(vertex) === 0);
 }
 
+/**
+ * Given a graph, return a Map whose keys are vertexes and whose
+ * values are that vertexes in-degree.
+ *
+ * A vertex's in-degree is the number of incoming edges to that vertex.
+ */
 function graphGetInDegrees(graph) {
-  let inDegrees = new Map(graphGetVertexes(graph).map(v => [v,0]));
+  let inDegrees = new Map(graphGetVertexes(graph).map(v => [v, 0]));
 
   return Object.values(graph).flat().reduce((map, v) => map.set(v, map.get(v) + 1), inDegrees);
 }
 
+/**
+ * Perform a topological sort on a graph
+ *
+ * This is iterative, breadth first, and non-destructive
+ */
 function topologicalSortBFS(graph) {
   let results = [];
   let inDegrees = graphGetInDegrees(graph);
@@ -53,6 +71,11 @@ function topologicalSortBFS(graph) {
   return results;
 }
 
+/**
+ * Perform a topological sort on a graph
+ *
+ * This is recursive, breadth first, and destructive
+ */
 function topologicalSortBFSRecursive(graph) {
   if (Object.keys(graph).length === 0) {
     return [];
@@ -65,32 +88,57 @@ function topologicalSortBFSRecursive(graph) {
 }
 
 /**
- * Given a graph, returns an array of vertexes that have been topologically sorted
-*/
+ * Perform a topological sort on a graph
+ *
+ * This is recursive, depth first, and non-destructive
+ */
 
-function topologicalSort(graph) {
+function topologicalSortDFS(graph) {
   let visited = new Set();
   let results = [];
 
   for (let vertex of Object.keys(graph)) {
     if (!visited.has(vertex)) {
-      dfs(graph, vertex, vertex => results.push(vertex), visited);
+      postOrderDFS(graph, vertex, vertex => results.push(vertex), visited);
     }
   }
 
   return results.reverse();
 }
 
-function dfs(graph, vertex, callbackFn, visited) {
-  if (visited.has(vertex)) {
+/**
+ * Given a graph, a starting vertex, and a callback function perform a
+ * post-order depth-first traversal of the graph. The callback function
+ * is called in the post-order position.
+ */
+function postOrderDFS(graph, startVertex, callbackFn, visited = new Set()) {
+  if (visited.has(startVertex)) {
     return;
   }
 
-  visited.add(vertex);
+  visited.add(startVertex);
 
-  for (let neighbor of graph[vertex]) {
-    dfs(graph, neighbor, callbackFn, visited);
+  for (let neighbor of graph[startVertex]) {
+    postOrderDFS(graph, neighbor, callbackFn, visited);
   }
 
-  callbackFn(vertex);
+  // Invoke the callback if we have it
+  if (typeof callbackFn === 'function') {
+    callbackFn(startVertex);
+  }
 }
+
+let vertexList = ['A', 'B', 'C', 'D', 'E', 'F'];
+let edgeList = [
+  ['A', 'B'],
+  ['A', 'C'],
+  ['C', 'D'],
+  ['D', 'B'],
+  ['D', 'E'],
+  ['F', 'C'],
+]
+
+let graph = graphToAdjacencyList(vertexList, edgeList);
+
+console.log('BFS:', topologicalSortBFS(graph));
+console.log('DFS:', topologicalSortDFS(graph));
