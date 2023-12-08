0772cf4e16cc6272b07091a5d0eccc4548b40105
Jesse Farmer <jesse@20bits.com>
2023-12-08 09:04:26 -0800

    misc

---
+++ backtracking/call-graph.js
@@ -0,0 +1,28 @@
+function main() {
+  printOne();
+  printTwo();
+  printThree();
+
+  return;
+}
+
+function printOne() {
+  console.log('One');
+  console.log('Another one');
+
+  return;
+}
+
+function printTwo() {
+  console.log('Two');
+
+  return;
+}
+
+function printThree() {
+  console.log('Three');
+
+  return;
+}
+
+main();
