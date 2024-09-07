49bcd99d3ebf60ebb6c5d292e051f4fd0e1ec9b1
Jesse Farmer <jesse@20bits.com>
2024-09-07 11:07:22 -0700

    [backtracking] Add subsequences_slice

--- backtracking/subsequences.js
+++ backtracking/subsequences.js
@@ -1,10 +1,25 @@
+function subsequences_slice(array, results = []) {
+  if (array.length === 0) {
+    console.log(results);
+    return;
+  }
+
+  let item = array[0];
+  let remaining = array.slice(idx + 1, array.length);
+
+  subsequences_slice(remaining, results.concat(item));
+  subsequences_slice(remaining, results);
+}
+
 function subsequences_concat(array, idx = 0, results = []) {
   if (array.length === idx) {
     console.log(results);
     return;
   }
 
-  subsequences_concat(array, idx + 1, results.concat(array[idx]));
+  let item = array[idx];
+
+  subsequences_concat(array, idx + 1, results.concat(item));
   subsequences_concat(array, idx + 1, results);
 }
 
@@ -14,7 +29,9 @@ function subsequences_push(array, idx = 0, results = [], final = []) {
     return;
   }
 
-  results.push(array[idx]);
+  let item = array[idx];
+
+  results.push(item);
   subsequences_push(array, idx + 1, results, final);
   results.pop();
   subsequences_push(array, idx + 1, results, final);
@@ -26,7 +43,9 @@ function subsequences_collect(array, idx = 0, results = [], output = []) {
     return output;
   }
 
-  results.push(array[idx]);
+  let item = array[idx];
+
+  results.push(item);
   subsequences_collect(array, idx + 1, results, output);
   results.pop();
   subsequences_collect(array, idx + 1, results, output);
